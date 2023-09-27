5cda19932dfbbff1d1861be389e182d3b81d71d3
Mario Camargo <44622444+mariocamargo-py@users.noreply.github.com>
2023-09-26 22:55:04 -0500

    Create demo.py

---
+++ demo.py
@@ -0,0 +1,13 @@
+from anthropic import Anthropic, HUMAN_PROMPT, AI_PROMPT
+
+anthropic = Anthropic(
+    # defaults to os.environ.get("ANTHROPIC_API_KEY")
+    api_key="key",
+)
+
+completion = anthropic.completions.create(
+    model="claude-2",
+    max_tokens_to_sample=300,
+    prompt=f"{HUMAN_PROMPT} how does a court case get to the Supreme Court?{AI_PROMPT}",
+)
+print(completion.completion)
